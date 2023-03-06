de38fcdc2964f4d75ab250c95a89069f0d8c8059
Evans <evansroysir@gmail.com>
2023-03-06 15:36:23 +0300

    Added components and styled in app.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <h1>To do List</h1>
+    <div className='todo-app'>
+      <TodoList />
     </div>
   );
 }
